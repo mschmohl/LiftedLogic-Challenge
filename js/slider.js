d43b7806c3cf6b7335d9e01525e97c65553b01c0
Melissa Schmohl <101364208+mschmohl@users.noreply.github.com>
2022-07-06 15:37:12 -0500

    Working carousel

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,43 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('slider__card');
+const totalSlides = slides.length;
 
-$(document).ready(function(){
-  $('.slider').slick({
-    setting-name: setting-value
+document.
+  getElementById('slider__button--next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+document.
+  getElementById('slider__button--prev')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
   });
 
-  $('.single-item').slick();
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('slider__card--visible');
+    slide.classList.add('slider__card--hidden');
+  }
 
-});
+  slides[slidePosition].classList.add('slider__card--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
